d65255d1d2e45e0112adb1f2b8334bfd174abccc
JBailey273 <jbailey@squire.enterprises>
2026-04-08 18:56:12 -0400

    Update 20260408_0027_add_invoice_billing_to_customers.py

--- apps/api/alembic/versions/20260408_0027_add_invoice_billing_to_customers.py
+++ apps/api/alembic/versions/20260408_0027_add_invoice_billing_to_customers.py
@@ -7,8 +7,8 @@ Create Date: 2026-04-08
 from alembic import op
 import sqlalchemy as sa
 
-revision = '0027'
-down_revision = '0026'
+revision = '20260408_0027'
+down_revision = '20260402_0025'
 branch_labels = None
 depends_on = None
 
