f954db471e24c8d9354211772bd45bcd321f920c
JBailey273 <jbailey@squire.enterprises>
2026-04-15 19:49:14 -0400

    Update and rename 20260415_0028_product_catalog_sort_order.py to 20260415_0029_product_catalog_sort_order.py

--- apps/api/alembic/versions/20260415_0029_product_catalog_sort_order.py
+++ apps/api/alembic/versions/20260415_0029_product_catalog_sort_order.py
@@ -1,14 +1,14 @@
 """add sort_order to product_catalog_items
 
-Revision ID: 20260415_0028
-Revises: 20260408_0027
+Revision ID: 20260415_0029
+Revises: 20260414_0028
 Create Date: 2026-04-15
 """
 from alembic import op
 import sqlalchemy as sa
 
-revision = "20260415_0028"
-down_revision = "20260408_0027"
+revision = "20260415_0029"
+down_revision = "20260414_0028"
 branch_labels = None
 depends_on = None
 
